f3d466916a6023ca2821a8ddfa21433cc021724b
peco <sorpwjdafh@naver.com>
2024-06-13 15:04:52 +0900

    fix(valid): validation message and exception message

--- src/common/exception-filter/prisma-exception.filter.ts
+++ src/common/exception-filter/prisma-exception.filter.ts
@@ -10,12 +10,13 @@ import { Prisma } from '@prisma/client';
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter implements GqlExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError): any {
+    console.log(exception);
     switch (exception.code) {
       case 'P2002': {
-        throw new ConflictException('Not Unique Email');
+        throw new ConflictException(exception.meta.target);
       }
       case 'P2003': {
-        throw new UnprocessableEntityException('Entity Not Exist');
+        throw new UnprocessableEntityException(exception.meta.field_name);
       }
       case 'P2025': {
         throw new NotFoundException(exception.meta.cause);
--- src/common/validation-message/length-validation.message.ts
+++ src/common/validation-message/length-validation.message.ts
@@ -4,11 +4,23 @@ export const lengthValidationMessage = (args: ValidationArguments) => {
   return `${args.property}은(는) ${args.constraints[0]} ~ ${args.constraints[1]}글자를 입력 해주세요.`;
 };
 
+export const lengthValidationDescription = (min: number, max: number) => {
+  return `${min} ~ ${max}글자를 입력 해주세요.`;
+};
+
 export const minLengthValidationMessage = (args: ValidationArguments) => {
   console.log(args.constraints);
   return `${args.property}은(는) 최소 ${args.constraints[0]}글자를 입력 해주세요.`;
 };
 
+export const minLengthValidationDescription = (min: number) => {
+  return `최소 ${min}글자를 입력 해주세요.`;
+};
+
 export const maxLengthValidationMessage = (args: ValidationArguments) => {
   return `${args.property}은(는) 최대 ${args.constraints[0]}글자를 입력 해주세요.`;
+};
+
+export const maxLengthValidationDescription = (max: number) => {
+  return `최대 ${max}글자까지 입력 해주세요.`;
 };
